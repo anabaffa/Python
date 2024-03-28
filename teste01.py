7fc499ddc7d8d5999c1e0963d88b2a105ea43d9e
Ana Baffa <163438308+anabaffa@users.noreply.github.com>
2024-03-28 16:52:31 -0300

    Update teste01.py

--- teste01.py
+++ teste01.py
@@ -1,11 +1,14 @@
 print('Olá, Mundo!')
 Olá, Mundo!
 
+
 print (7+4)
 11
 
+
 print('7'+'4')
 74
+
 
 nome = 'Maria'
 idade = 25
@@ -13,15 +16,27 @@ peso = 74.8
 print(nome, idade, peso)
 Maria 25 74.8
 
+
 nome = input('Qual seu nome? ')
 idade = input('Qual sua idade? ')
 peso = input('Qual seu peso? ')
 print(nome, idade, peso)
 
+
 nome = input('Qual seu nome ')
 print('Olá ', nome, '! Prazer em te conhecer!')
+
 
 dia = input('Dia = ')
 mes = input('Mês = ')
 ano = input('Ano = ')
 print('Você nasceu no dia ', dia, 'de ', mes, 'de ', ano, 'Correto?')
+
+
+n1 = input('Primeiro número ')
+n2 = input ('Segundo número ')
+n1 = int(n1)
+n2 = int(n2)
+soma = n1 + n2
+print('A soma é ', soma)
+
