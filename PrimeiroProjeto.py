82c3a0dae623c8c73d6f80d639d48eccc0f41fad
Ana Baffa <163438308+anabaffa@users.noreply.github.com>
2024-03-28 16:32:59 -0300

    Update and rename PrimeiroProjeto.py to teste01.py

--- PrimeiroProjeto.py
+++
@@ -1,8 +0,0 @@
-print('Olá, Mundo!')
-Olá, Mundo!
-
-7+4
-11
-
-print('7'+'4')
-74
---
+++ teste01.py
@@ -0,0 +1,20 @@
+print('Olá, Mundo!')
+Olá, Mundo!
+
+print (7+4)
+11
+
+print('7'+'4')
+74
+
+nome = 'Maria'
+idade = 25
+peso = 74.8
+print(nome, idade, peso)
+Maria 25 74.8
+
+nome = input('Qual seu nome? ')
+idade = input('Qual sua idade? ')
+peso = input('Qual seu peso? ')
+print(nome, idade, peso)
+
